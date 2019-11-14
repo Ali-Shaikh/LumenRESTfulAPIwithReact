78f39dbbbfe81f128de7f5c9033b8cc5dcc96c68
Ali Shaikh <ali.az.shaikh@gmail.com>
2019-11-14 11:06:43 +0500

    Configure Docker for front-end and back-end #4

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>React x Lumen by Ali Shaikh!</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    React x Lumen by Ali Shaikh!
                 </div>
 
                 <div class="links">
