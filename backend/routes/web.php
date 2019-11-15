b17d1f026a5dd142252a9e89f534cbc42f0fc2fe
Ali Shaikh <ali.az.shaikh@gmail.com>
2019-11-15 22:19:46 +0500

    #7 - Created application API routes and routes for Books

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -10,7 +10,30 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+/** @var \Laravel\Lumen\Routing\Router $router */
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+// API Routes
+$router->group(['prefix' => 'api'], function () use ($router) {
+    // API v1 Routes
+    $router->group(['prefix' => 'v1'], function () use ($router) {
+
+        $router->get('/', function () use ($router) {
+            return 'Welcome to &quot;Lumen x React&quot; API v1 by <b>Ali Shaikh</b>!';
+        });
+
+        $router->get('books', ['uses' => 'BookController@getAllBooks']);
+
+        $router->get('books/{id}', ['uses', 'BookController@getBookByID']);
+
+        $router->post('books', ['uses', 'BookController@create']);
+
+        $router->delete('books/{id}', ['uses', 'BookController@delete']);
+
+        $router->put('books/{id}', ['uses', 'BookController@update']);
+
+    });
 });
