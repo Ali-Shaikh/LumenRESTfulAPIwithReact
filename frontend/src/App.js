e45fdc1de43d7aaed89f763eed06bc1c6465d122
Ali Shaikh <ali.az.shaikh@gmail.com>
2019-11-24 22:12:16 +0500

    #12 Updated ESLint config and installed Prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,31 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-                    Welcome to React x Lumen App by Ali Shaikh!
-          </p>
-          <p>
-                    Edit
-            {' '}
-            <code>src/App.js</code>
-            {' '}
-                    and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-                    Learn React
-          </a>
-        </header>
-      </div>
-    );
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to React x Lumen App by Ali Shaikh!</p>
+        <p>
+          Edit
+          <code> src/App.js </code>
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
